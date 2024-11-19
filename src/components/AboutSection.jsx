d979f2aaa213e519e066ddf673ac72a5553830cf
Babatunde Maryam Funmilayo <dwisemaryam@gmail.com>
2024-11-18 20:54:40 -0600

    updated About section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import aboutImage from "../assets/aboutImage.png";
+import aboutImage2 from "../assets/aboutImage2.png";
 
 const AboutSection = () => {
   return (
@@ -56,6 +57,56 @@ const AboutSection = () => {
             alt="About Us"
             className="w-full md:w-[85%] lg:w-[75%] h-auto object-cover"
           />
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row items-center mt-10">
+        {/* Image Section */}
+        <div className="md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+          <img
+            src={aboutImage2}
+            alt="Objectives"
+            className="w-full md:w-[85%] lg:w-[75%] h-auto object-cover"
+          />
+        </div>
+
+        {/* Objectives Section */}
+        <div className="md:w-1/2 md:pl-10 space-y-6">
+          <h2 className="text-3xl text-[#720034] font-bold">Objectives</h2>
+          <p className="mt-4">
+            1. To provide world-class education and training programs in
+            distributed ledger systems, Web3, and emerging technologies,
+            empowering individuals and organizations with cutting-edge knowledge
+            and skills.
+            <br />
+            <br />
+            2. To foster a vibrant community of innovators, entrepreneurs, and
+            experts committed to driving sustainable development and economic
+            growth through the application of decentralized technologies and
+            other advanced digital solutions.
+            <br />
+            <br />
+            3. To promote the adoption and utilization of decentralized systems,
+            Web3 innovations, and emerging tech solutions across Africa, aiming
+            to enhance efficiency, transparency, and inclusivity in various
+            sectors.
+            <br />
+            <br />
+            4. To support the development of innovative technological solutions
+            that address real-world challenges, with a focus on digital
+            transformation and next-generation infrastructure.
+            <br />
+            <br />
+            5. To facilitate collaboration and networking among enthusiasts and
+            professionals in the decentralized tech space, encouraging knowledge
+            sharing, partnerships, and the development of forward-thinking
+            solutions.
+            <br />
+            <br />
+            6. To drive positive social and economic impact by leveraging
+            decentralized technologies and digital innovations for initiatives
+            that foster community development, financial inclusion, and
+            sustainable growth.
+          </p>
         </div>
       </div>
     </section>
