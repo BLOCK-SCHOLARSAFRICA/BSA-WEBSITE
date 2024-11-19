c90ea6822f9c619801f2824277c21c1f45da01fe
Babatunde Maryam Funmilayo <dwisemaryam@gmail.com>
2024-11-19 17:15:42 -0600

    update Events section

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -1,14 +1,28 @@
-// src/components/AboutSection.js
 import React from "react";
+import quizNight from "../assets/Quiz Night.png";
+import techTour from "../assets/Tech tour 1.png";
+import weeklyProgram from "../assets/Weekly engagement.png";
 
 const EventSection = () => {
   return (
     <section id="events" className="py-10 px-6 md:px-14 bg-white">
-      <h2 className="text-3xl text-[#720034] font-bold">Events</h2>
-      <div >
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+      <h2 className="text-3xl text-[#720034] font-bold mb-6">Events</h2>
+      <div className="flex flex-col md:flex-row gap-6 items-center justify-center">
+        <img
+          src={quizNight}
+          alt="Quiz Night"
+          className="w-full md:w-1/3 h-auto object-cover rounded-[40px] shadow-md"
+        />
+        <img
+          src={techTour}
+          alt="Tech Tour"
+          className="w-full md:w-1/3 h-auto object-cover rounded-[40px] shadow-md"
+        />
+        <img
+          src={weeklyProgram}
+          alt="Weekly Program"
+          className="w-full md:w-1/3 h-auto object-cover rounded-[40px] shadow-md"
+        />
       </div>
     </section>
   );
