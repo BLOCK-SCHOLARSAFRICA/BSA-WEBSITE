706ef9344f2fad4b6a0e6d1b7fb61e0f0d50426e
Justjoseph0 <iamonowu@gmail.com>
2025-03-31 19:02:29 +0100

    Added Tailwind CSS to Vite project

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,7 @@
 // src/components/Sections.js
 export { default as HeroSection } from "./HeroSection";
 export { default as AboutSection } from "./AboutSection";
-// export { default as EventSection } from "./EventSection";
+export { default as EventSection } from "./EventSection";
 export { default as Community } from "./Community";
 export { default as ContactSection } from "./ContactSection";
 export {default as Partnership} from "./Partnership"
