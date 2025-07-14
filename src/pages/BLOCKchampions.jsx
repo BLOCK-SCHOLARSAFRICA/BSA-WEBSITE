0c3abaf377d1c45d2ef7e7d908356dc5ed684108
Justjoseph0 <iamonowu@gmail.com>
2025-07-14 17:16:42 +0100

    Update padding in BLOCKchampions page for improved mobile responsiveness and layout consistency.

--- src/pages/BLOCKchampions.jsx
+++ src/pages/BLOCKchampions.jsx
@@ -171,7 +171,7 @@ const BLOCKchampions = () => {
       </div>
 
       {/* core champions section */}
-      <div className="min-h-screen bg-[#212020] text-white p-8">
+      <div className="min-h-screen bg-[#212020] text-white lg:p-8 p-4">
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-12">
             {/* Starter Champion */}
