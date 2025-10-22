9cdc5ab71c5c08e40aedea210d6a2f323b871aec
milytoh <milytohgold@gmail.com>
2025-10-22 03:52:09 -0700

    update official partner

--- src/components/conference/SponsorPartners.jsx
+++ src/components/conference/SponsorPartners.jsx
@@ -60,6 +60,47 @@ const SponsorPartners = (prop) => {
   return (
     <section className="bg-white">
       <div className="flex flex-col items-center ">
+        {/* official partners */}
+        <div
+          className="flex flex-col justify-center gap-[20px] px-6 items-center bg-[#407AFB] text-white rounded rounded-3xl w-[340px]
+         mx-10  my-10 h-[560px] md:flex-row md:w-5/6 md:h-96 md:justify-between"
+        >
+          <div className="flex flex-col items-center  gap-6 md:items-start md:pl[200px] md:pl-10 ">
+            <p
+              className="text-[26px]  font-extrabold md:text-left md:text-[35px] "
+              style={{ fontFamily: "Poppins" }}
+            >
+              {" "}
+              Official Partner
+            </p>
+            <p
+              className="text-sm text-[14px] text-center md:w-[300px] md:text-left  md:ml[200px] "
+              style={{ fontFamily: "Poppins" }}
+            >
+              Powered By Block-Scholars Africa(BSA) & Teen Girls In
+              Blockchain(TGIB),  Block-Scholars Africa & Teen Girls in
+              Blockchain proudly power AWDEC2025, championing inclusion,
+              innovation, and the empowerment of young Africans-especially
+              wome-in blockchain, technology, and the digital economy across
+              Africa.
+            </p>
+          </div>
+
+          <div
+            className="flex items-center justify-center bg-white h-[258px] w-[280px] rounded rounded-3xl mx-auto md:w-[370px] md:h-[300px]"
+            data-aos="fade-up"
+          >
+            {officialLogo.map((logo, index) => (
+              <img
+                key={index}
+                src={logo}
+                className={`absolute w-[199px] h-[180px] object-contain transition-opacity duration-1000 md:w-[250px] md:h-[200px] ${
+                  index === office ? "opacity-100" : "opacity-0"
+                }`}
+              />
+            ))}
+          </div>
+        </div>
         {/* community partners */}
         <div
           className="flex flex-col justify-center gap-[70px] px-6 items-center bg-[#407AFB] text-white rounded rounded-3xl w-[340px]
@@ -98,45 +139,6 @@ const SponsorPartners = (prop) => {
               />
             ))}
             {/* <img src={partner1}></img> */}
-          </div>
-        </div>
-
-        {/* official partners */}
-        <div
-          className="flex flex-col justify-center gap-[70px] px-6 items-center bg-[#407AFB] text-white rounded rounded-3xl w-[340px]
-         mx-10  my-10 h-[560px] md:flex-row md:w-5/6 md:h-96 md:justify-between"
-        >
-          <div className="flex flex-col items-center  gap-6 md:items-start md:pl[200px] md:pl-10 ">
-            <p
-              className="text-[26px]  font-extrabold md:text-left md:text-[35px] "
-              style={{ fontFamily: "Poppins" }}
-            >
-              {" "}
-              Official Partner
-            </p>
-            <p
-              className="text-sm text-[14px] text-center md:w-[300px] md:text-left  md:ml[200px] "
-              style={{ fontFamily: "Poppins" }}
-            >
-              Teen Girls in Blockchain serves as our official partner for
-              AWDEC2025, championing the inclusion and empowerment of young
-              women in blockchain, technology, and innovation across Africa
-            </p>
-          </div>
-
-          <div
-            className="flex items-center justify-center bg-white h-[258px] w-[280px] rounded rounded-3xl mx-auto md:w-[370px] md:h-[300px]"
-            data-aos="fade-up"
-          >
-            {officialLogo.map((logo, index) => (
-              <img
-                key={index}
-                src={logo}
-                className={`absolute w-[199px] h-[180px] object-contain transition-opacity duration-1000 md:w-[250px] md:h-[200px] ${
-                  index === office ? "opacity-100" : "opacity-0"
-                }`}
-              />
-            ))}
           </div>
         </div>
 
